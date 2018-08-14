2d0ab6628b34788b09b95e4264a8103979450550
lujinbiao <jinbiao.lu@pactera.com>
2018-08-14 10:20:52 +0800

    create new project

---
+++ CircleImageApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+a
+1app/src/main/java/com/road/demo/MainActivity.java,d\4\d47366611cabf201232d68c2ba96233dfdddca49
+p
+@circleimageview/src/main/java/com/road/demo/CircleImageView.java,e\b\eb91cfa91d639033460992457d8fb86cee7944bb
+Y
+)app/src/main/res/layout/activity_main.xml,7\6\76b07e40f24cce093536731477540f12e7da3cb6
---
+++ CircleImageApp/circleimageview/src/main/java/com/road/demo/CircleImageView.java
@@ -0,0 +1,201 @@
+package com.road.demo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CircleImageView extends ImageView {
+    private static final ScaleType SCALE_TYPE = ScaleType.CENTER_CROP;
+
+    private static final Bitmap.Config BITMAP_CONFIG = Bitmap.Config.ARGB_8888;
+
+    /**
+     * 默认ColorDrawable的宽和高
+     */
+    private static final int COLOR_DRAWABLE_DIMENSION = 0;
+
+    /**
+     * 绘制图片的方形
+     */
+    private final RectF mDrawableRect = new RectF();
+
+    /**
+     * 对图片进行缩放和移动的矩阵
+     */
+    private final Matrix mShaderMatrix = new Matrix();
+
+    /**
+     * 画笔
+     */
+    private final Paint mBitmapPaint = new Paint();
+
+    private Bitmap mBitmap;
+
+    /**
+     * 图片渲染器
+     */
+    private BitmapShader mBitmapShader;
+    /**
+     * 图片宽度
+     */
+    private int mBitmapWidth;
+    /**
+     * 图片高度
+     */
+    private int mBitmapHeight;
+    /**
+     * 图片半径
+     */
+    private float mDrawableRadius;
+
+    private boolean mReady;
+    /**
+     * 内边距
+     */
+    private boolean mSetupPending;
+
+    public CircleImageView(Context context) {
+        super(context);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mReady = true;
+
+        if (mSetupPending) {
+            setup();
+            mSetupPending = false;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (getDrawable() == null) {
+            return;
+        }
+
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, mDrawableRadius, mBitmapPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        setup();
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        mBitmap = bm;
+        setup();
+    }
+
+    @Override
+    public void setImageDrawable(Drawable drawable) {
+        super.setImageDrawable(drawable);
+        mBitmap = getBitmapFromDrawable(drawable);
+        setup();
+    }
+
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        mBitmap = getBitmapFromDrawable(getDrawable());
+        setup();
+    }
+
+    private Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        try {
+            Bitmap bitmap;
+            if (drawable instanceof ColorDrawable) {
+                bitmap = Bitmap.createBitmap(COLOR_DRAWABLE_DIMENSION, COLOR_DRAWABLE_DIMENSION, BITMAP_CONFIG);
+            } else {
+                bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),
+                        BITMAP_CONFIG);
+            }
+
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (OutOfMemoryError e) {
+            return null;
+        }
+    }
+
+    private void setup() {
+        if (!mReady) {
+            mSetupPending = true;
+            return;
+        }
+
+        if (mBitmap == null) {
+            return;
+        }
+
+        /**
+         * TileMode.CLAMP:用边缘色彩填充多余空间
+         * TileMode.REPEAT:重复原图像来填充多余空间
+         * TileMode.MIRROR:重复使用镜像模式的图像来填充多余空间
+         */
+        mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+
+        mBitmapPaint.setAntiAlias(true);
+        mBitmapPaint.setShader(mBitmapShader);
+
+        mBitmapHeight = mBitmap.getHeight();
+        mBitmapWidth = mBitmap.getWidth();
+
+        mDrawableRect.set(0, 0, getWidth(), getHeight());
+        mDrawableRadius = Math.min(mDrawableRect.height() / 2, mDrawableRect.width() / 2);
+
+        updateShaderMatrix();
+        invalidate();
+    }
+
+    private void updateShaderMatrix() {
+        float scale;
+        float dx = 0;
+        float dy = 0;
+
+        mShaderMatrix.set(null);
+
+        if (mBitmapWidth * mDrawableRect.height() > mDrawableRect.width() * mBitmapHeight) {
+            scale = mDrawableRect.height() / (float) mBitmapHeight;
+            dx = (mDrawableRect.width() - mBitmapWidth * scale) * 0.5f;
+        } else {
+            scale = mDrawableRect.width() / (float) mBitmapWidth;
+            dy = (mDrawableRect.height() - mBitmapHeight * scale) * 0.5f;
+        }
+
+        mShaderMatrix.setScale(scale, scale);
+        mShaderMatrix.postTranslate((int) (dx + 0.5f), (int) (dy + 0.5f));
+
+        mBitmapShader.setLocalMatrix(mShaderMatrix);
+    }
+
+}
